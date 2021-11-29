4f4b44b8108c31405a3ed50c0281d78f35ed7598
Rynoster <Ryno.Meiboom@gmail.com>
2021-11-29 13:22:03 +0200

    tut done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rmeiboom <rmeiboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/22 10:02:02 by rmeiboom      #+#    #+#                 */
-/*   Updated: 2021/11/29 10:41:59 by rmeiboom      ########   odam.nl         */
+/*   Updated: 2021/11/29 11:01:46 by rmeiboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <pthread.h>
 
 #define SUCCESS 0
+#define ERROR 1
 
 int mails = 0;
 pthread_mutex_t mutex;
@@ -23,23 +24,51 @@ void routine()
 {
 	for (int i = 0; i < 1000000; i++)
 	{
+		pthread_mutex_lock(&mutex);
 		++mails;
+		pthread_mutex_unlock(&mutex);
 	}
 	// printf
 }
 
+int	create_threads(pthread_t *threads, const int number_of_threads)
+{
+	int	i;
+
+	i = 0;
+	while (i < number_of_threads)
+	{
+		if (pthread_create(&threads[i], NULL, (void *)&routine, NULL) != SUCCESS)
+			return (ERROR);
+		++i;
+		printf("thread started: %d\n", i);
+	}
+	return (SUCCESS);
+}
+
+int	join_threads(pthread_t *threads, const int number_of_threads)
+{
+	int	i;
+
+	i = 0;
+	while (i < number_of_threads)
+	{
+		if (pthread_join(threads[i], NULL) != SUCCESS)
+			return (ERROR);
+		++i;
+		printf("thread has finished: %d\n", i);
+	}
+	return (SUCCESS);
+}
+
 int	main(void)
 {
-	pthread_t p1, p2;
+	pthread_t threads[4];
 	pthread_mutex_init(&mutex, NULL);
-	if (pthread_create(&p1, NULL, (void *)&routine, NULL) != SUCCESS)
-		return (1);
-	if (pthread_create(&p2, NULL, (void *)&routine, NULL) != SUCCESS)
-		return (2);
-	if (pthread_join(p1, NULL) != SUCCESS)
-		return (3);
-	if (pthread_join(p2, NULL) != SUCCESS)
-		return (4);
+	if (create_threads(threads, 4) == ERROR)
+		return 2;
+	if (join_threads(threads, 4) == ERROR)
+		return 3;
 	printf("Number of mails: %d\n", mails);
 	pthread_mutex_destroy(&mutex);
 	return (SUCCESS);

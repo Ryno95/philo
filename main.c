615e03847fde4b8deab8f129d3a305d9c91e4730
Rynoster <Ryno.Meiboom@gmail.com>
2021-11-29 10:43:09 +0200

    basic mutex intro

--- main.c
+++ main.c
@@ -6,14 +6,41 @@
 /*   By: rmeiboom <rmeiboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/22 10:02:02 by rmeiboom      #+#    #+#                 */
-/*   Updated: 2021/11/27 10:59:24 by rmeiboom      ########   odam.nl         */
+/*   Updated: 2021/11/29 10:41:59 by rmeiboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+
+#define SUCCESS 0
+
+int mails = 0;
+pthread_mutex_t mutex;
+
+void routine()
+{
+	for (int i = 0; i < 1000000; i++)
+	{
+		++mails;
+	}
+	// printf
+}
 
 int	main(void)
 {
-	printf("Let's goooooo!\n");
-	return (0);
+	pthread_t p1, p2;
+	pthread_mutex_init(&mutex, NULL);
+	if (pthread_create(&p1, NULL, (void *)&routine, NULL) != SUCCESS)
+		return (1);
+	if (pthread_create(&p2, NULL, (void *)&routine, NULL) != SUCCESS)
+		return (2);
+	if (pthread_join(p1, NULL) != SUCCESS)
+		return (3);
+	if (pthread_join(p2, NULL) != SUCCESS)
+		return (4);
+	printf("Number of mails: %d\n", mails);
+	pthread_mutex_destroy(&mutex);
+	return (SUCCESS);
 }

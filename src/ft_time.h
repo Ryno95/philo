ebc2f107c0daccede4213b3df1891bd30aa74aae
Rynoster <Ryno.Meiboom@gmail.com>
2021-12-06 14:00:35 +0200

    added header for max/linux int

--- src/ft_time.h
+++ src/ft_time.h
@@ -6,12 +6,14 @@
 /*   By: rmeiboom <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/06 11:20:47 by rmeiboom      #+#    #+#                 */
-/*   Updated: 2021/12/06 13:11:45 by rmeiboom      ########   odam.nl         */
+/*   Updated: 2021/12/06 14:00:20 by rmeiboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_TIME_H
 # define FT_TIME_H
+
+#include <stdlib.h>
 
 u_int64_t	get_timestamp(void);
 void		sleep_ms(u_int64_t sleep_ms);

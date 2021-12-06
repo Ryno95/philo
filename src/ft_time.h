b01e07d70af260481893ad25efe2652a856fd77a
Rynoster <Ryno.Meiboom@gmail.com>
2021-12-06 14:30:29 +0200

    normed

--- src/ft_time.h
+++ src/ft_time.h
@@ -6,14 +6,14 @@
 /*   By: rmeiboom <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/06 11:20:47 by rmeiboom      #+#    #+#                 */
-/*   Updated: 2021/12/06 14:25:06 by rmeiboom      ########   odam.nl         */
+/*   Updated: 2021/12/06 14:30:02 by rmeiboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_TIME_H
 # define FT_TIME_H
 
-#	include "defines.h"
+# include "defines.h"
 
 t_time_ms	get_timestamp(void);
 void		sleep_ms(t_time_ms sleep_ms);
